a5b13e10ddf9561ba2dea8bb00717760088205eb
jxxny <axcv__re@naver.com>
2022-10-05 01:37:59 +0900

    제발............

--- capston/src/empinfo.js
+++ capston/src/empinfo.js
@@ -6,6 +6,7 @@ import "./empinfo.css";
 const App = () => {
 	return (
 		<div className='empInfo'>
+			<div className='plzEmp'>
 			<div>
 				<div class="wrapimg">
 					<img class="empimg" src={testimg} alt="이미지"/>
@@ -17,23 +18,41 @@ const App = () => {
 			<p className='dept'>경영관리본부</p>
 		</div>
 
+		<div class="emp">
 			<table class="firstTable">
 				<tbody class="infoList">
 					<tr>
-					<td>ㆍ사번</td><td>ㆍ재직상태</td><td>ㆍ부서명</td><td>ㆍ직책</td><td>ㆍ사원구분</td>
+						<td>ㆍ사번</td>
+						<td>ㆍ재직상태</td>
+						<td>ㆍ부서명</td>
+						<td>ㆍ직책</td>
+						<td>ㆍ사원구분</td>
 					</tr>
 					<tr>
-					<td>nnnnnn</td><td>재직</td><td>경영지원부</td><td>사원</td><td>임원</td>
+						<td>nnnnnn</td>
+						<td>재직</td>
+						<td>경영지원부</td>
+						<td>사원</td>
+						<td>임원</td>
 					</tr>
 					<br></br>
 					<tr>
-					<td>ㆍ생년월일</td><td>ㆍ입사일</td><td>ㆍ근속기간</td><td>ㆍ현부서발령일</td><td>ㆍ퇴사일</td>
+						<td>ㆍ생년월일</td>
+						<td>ㆍ입사일</td>
+						<td>ㆍ근속기간</td>
+						<td>ㆍ현부서발령일</td>
+						<td>ㆍ퇴사일</td>
 					</tr>
 					<tr>
-					<td>2000-00-00</td><td>2022-09-01</td><td>n년 n개월</td><td>2022-10-01</td><td></td>
+						<td>2000-00-00</td>
+						<td>2022-09-01</td>
+						<td>n년 n개월</td>
+						<td>2022-10-01</td>
+						<td></td>
 					</tr>
 				</tbody>
 			</table>
+		</div>
 
 			<hr class="empFirstLine" align="left"></hr>
 
@@ -42,6 +61,7 @@ const App = () => {
 			<div class="account">계좌</div>
 
 			<hr class="empFirstLine" align="left"></hr>
+		</div>
 		</div>
 	);
 };
