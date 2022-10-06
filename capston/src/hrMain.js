fcdb8fb3d88cce1820d706c2c7505f0c6830f904
Kimjinwon <59737252+hdev1004@users.noreply.github.com>
2022-10-06 23:48:46 +0900

    hrMain페이지 업데이트 - hdev1004 	modified:   capston/src/css/hrMain.css 	modified:   capston/src/hrMain.js

--- capston/src/hrMain.js
+++ capston/src/hrMain.js
@@ -2,86 +2,92 @@ import React from 'react';
 
 import "./css/hrMain.css";
 
+import Basic from "../src/img/basic.png";
+import Profile from "../src/img/profile.png";
+
 const App = () => {
 	return (
         <div className='hrMain'>
-           <div className="container">
-                <div className="user_container">
-                    <h4>사용자 정보</h4>
-                    <div className="userInfo">
-                        <div className="userImg">
-                            <img src="img/profile.png" alt="userImg"/>
+           <div className="hrMain_container">
+                <div className="hrMain_Flex">
+                    <div className="hrMain_user_container">
+                        <h4>사용자 정보</h4>
+                        <div className="hrMain_userInfo">
+                            <div className="hrMain_userImg">
+                                <img src={Profile} alt="userImg"/>
+                            </div>
+                            <div className="hrMain_userDetail">
+                                <li className="hrMain_userName">김명지</li>
+                                <li>2022771010</li>
+                                <li>경영관리본부</li>
+                                <li>경영기획부</li>
+                                <li>총무인사팀</li>
+                            </div>
                         </div>
-                        <div className="userDetail">
-                            <li className="userName">김명지</li>
-                            <li>2022771010</li>
-                            <li>경영관리본부</li>
-                            <li>경영기획부</li>
-                            <li>총무인사팀</li>
+                    </div>
+                    <div className="hrMain_work_container">
+                        <h4>근무 정보</h4>
+                        <div className="hrMain_workInfo">
+                            <ul>
+                                <li className='hrMain_today'>2022.09.29 목</li>
+                                <li>출근	2022-07-29	08:49</li>
+                                <li>연차 <progress value="20" max="100"></progress></li>
+                                <p> 잔여 연차 12개 / 총 연차 5개 <br/> 2022년 17개 갱신 </p>
+                            </ul>
+                        </div>
+                        <div className="hrMain_history">
+                            <h4>신청내역</h4>
+                            <button className="hrMain_history_btn">
+                                <img className="hrMain_button_img" src={Basic} alt="detail"/>
+                            </button>
+                        </div>
+                        <div className="hrMain_historyList">
+                            <table>
+                                <th>신청종류</th>
+                                <th>신청일자</th>
+                                <th>처리</th>
+                                <th>접수일자</th>
+                                <tr>
+                                    <td>연차휴가</td>
+                                    <td>2022.07.28</td>
+                                    <td>완료</td>
+                                    <td>2022.07.28</td>
+                                </tr>
+                                <tr>
+                                    <td>연차휴가</td>
+                                    <td>2022.07.15</td>
+                                    <td>완료</td>
+                                    <td>2022.07.15</td>
+                                </tr>
+                                <tr>
+                                    <td>연차휴가</td>
+                                    <td>2022.05.15</td>
+                                    <td>완료</td>
+                                    <td>2022.05.15</td>
+                                </tr>
+                                <tr>
+                                    <td>출장</td>
+                                    <td>2022.03.15</td>
+                                    <td>완료</td>
+                                    <td>2022.03.15</td>
+                                </tr>
+                                <tr>
+                                    <td>연차휴가</td>
+                                    <td>2022.02.15</td>
+                                    <td>완료</td>
+                                    <td>2022.02.15</td>
+                                </tr>
+                            </table>
                         </div>
                     </div>
                 </div>
-                <div className="work_container">
-                    <h4>근무 정보</h4>
-                    <div className="workInfo">
-                        <ul>
-                            <li className='today'>2022.09.29 목</li>
-                            <li>출근	2022-07-29	08:49</li>
-                            <li>연차 <progress value="20" max="100"></progress></li>
-                            <p> 잔여 연차 12개 / 총 연차 5개 <br/> 2022년 17개 갱신 </p>
-                        </ul>
-                    </div>
-                    <div className="history">
-                        <h4>신청내역</h4>
-                        <button className="history_btn">
-                            <img className="button_img" src="img/basic.png" alt="detail"/>
-                        </button>
-                    </div>
-                    <div className="historyList">
-                        <table>
-                            <th>신청종류</th>
-                            <th>신청일자</th>
-                            <th>처리</th>
-                            <th>접수일자</th>
-                            <tr>
-                                <td>연차휴가</td>
-                                <td>2022.07.28</td>
-                                <td>완료</td>
-                                <td>2022.07.28</td>
-                            </tr>
-                            <tr>
-                                <td>연차휴가</td>
-                                <td>2022.07.15</td>
-                                <td>완료</td>
-                                <td>2022.07.15</td>
-                            </tr>
-                            <tr>
-                                <td>연차휴가</td>
-                                <td>2022.05.15</td>
-                                <td>완료</td>
-                                <td>2022.05.15</td>
-                            </tr>
-                            <tr>
-                                <td>출장</td>
-                                <td>2022.03.15</td>
-                                <td>완료</td>
-                                <td>2022.03.15</td>
-                            </tr>
-                            <tr>
-                                <td>연차휴가</td>
-                                <td>2022.02.15</td>
-                                <td>완료</td>
-                                <td>2022.02.15</td>
-                            </tr>
-                        </table>
-                    </div>
+
+                <div className="hrMain_notice_container">
+                    <h4>공지사항</h4>
+                    <div className="hrMain_notice"></div>
                 </div>
             </div>
             
-            <div className="notice_container">
-                    <h4>공지사항</h4>
-                    <div className="notice"></div>
-                </div>
         </div>
 	);
 };
