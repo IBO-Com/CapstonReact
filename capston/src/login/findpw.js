f53d5c4ccc8f67ed1b81f3c8bf257e94774a108a
jxxny <axcv__re@naver.com>
2022-10-09 00:53:35 +0900

    비밀번호 찾기 + 로그인 비밀번호 쪽 폴더정리

---
+++ capston/src/login/findpw.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import "../css/login/findpw.css"
+
+import { Link } from 'react-router-dom';
+
+const App = () => {
+    return (
+        <div>
+            <div className='findpwBox'>
+                <div>
+                    <span className='findpwTitle'>비밀번호 찾기</span>
+                    <span className='findpwContent'>비밀번호는 이름, 사번, 이메일을 통해 찾을 수 있습니다.</span>
+                </div>
+
+
+                <div className='userFindpw'>
+                    <form className="findpwInput">
+                        <div className="findpwInputName">
+                            <input type="text" name="findpwName" id="findpwName" autocomplete="off" placeholder="이름을 입력해주세요." />
+                        </div>
+                        <div className="findpwInputSabun">
+                            <input type="text" name="findpwSabun" id="findpwSabun" autocomplete="off" placeholder="사번을 입력해주세요." />
+                        </div>
+                        <div className="findpwInputEmail">
+                            <input type="text" name="findpwEmail" id="findpwEmail" autocomplete="off" placeholder="이메일을 입력해주세요." />
+                        </div>
+
+                        <Link to="/findpw">
+                            <button className="findpwBtn" type="button">
+                                비밀번호 찾기
+                            </button>
+                        </Link>
+                    </form>
+                </div>
+            </div>
+        </div>
+
+
+        //<Empinfo />
+    );
+};
+
+export default App;
---
+++ capston/src/login/login.js
@@ -0,0 +1,71 @@
+import React, { useEffect, useState } from 'react';
+import security_icon from "../img/iboLogoImg.png";
+import "../css/login/login.css"
+import { Link } from 'react-router-dom';
+
+
+const App = () => {
+  const [isLogin, setIsLogin] = useState(false);
+
+  const Submit = (e) => {
+    e.preventDefault();
+    //로그인이 되었다면 true 
+    setIsLogin(true);
+  }
+
+	return (
+    isLogin == false ? (  //로그인이 안되었다면
+      <div className='LoginApp'>
+        <div class="left_ibo">
+          <img class="logo" src={security_icon} alt="IBO"/>
+          <h2 class="com_name">IBO</h2>
+          <p class="left">
+            Welcome to IBO! <br/> Enter your account
+          </p>
+        </div>
+
+        <div class="userlogin">
+          <form class="input" onSubmit={Submit}>
+            <div class="inputid">
+              <input type="text" name="userid" id="userid" autocomplete="off"
+                placeholder="아이디를 입력해주세요."/>
+            </div>
+            <div class="inputpw">
+              <input type="password" name="userpw" id="userpw" autocomplete="off"
+                placeholder="비밀번호를 입력해주세요."/>
+            </div>
+            <div>
+              <span class="rememid"><input type="checkbox"/> 아이디 저장</span>
+            </div>
+            <br/>
+            <div>
+              <Link to="/MainNav">
+                <input id="loginbtn" name="loginbtn" value="LOGIN" />
+              </Link>
+            </div>
+            <br/>
+            <div class="findbtn">
+              <Link to="/findpw">
+                <span className='/findpw'>비밀번호를 잊어버리셨나요?</span>
+              </Link>
+              
+            </div>
+          </form>
+        </div>
+		  </div>
+    ) : ( //로그인이 되었다면
+    
+      <div>
+        
+      {/*<MainNav/>
+        네비게이션 이동 수정.
+      */}  
+      
+
+      </div>
+    )
+		
+	);
+};
+
+export default App;
