af6fd242e01391fd1242842b7f9c65942645146d
syLee <siyeon00@kakao.com>
2022-10-10 02:19:12 +0900

    FEAT : 사원정보현황 페이지 추가 body : 페이지 Link 추가가 안됐으므로 http://localhost:3000/mainNav/hrorganization/ 로 주소창에 타이핑하여 이동한다.

--- capston/src/HRorganization/HRorganization.js
+++ capston/src/HRorganization/HRorganization.js
@@ -1,29 +1,133 @@
 import React, { useState } from "react";
-import DatePicker from "react-datepicker";
 import "../css/HRorganization/HRorganization.css";
 import "react-datepicker/dist/react-datepicker.css";
 import { ko } from "date-fns/esm/locale";
+import Select from "@mui/material/Select";
+import {DatePicker, MuiPickersUtilsProvider} from "@material-ui/pickers";
+import DateFnsUtils from "@date-io/date-fns";
+import FormControl from "@mui/material/FormControl";
+import { Button, MenuItem, TextField } from "@mui/material";
+import HrInfoTable from "./HrInfoTable";
+import "../css/HRorganization/HrInfoTable.css";
+import koLocale from "date-fns/locale/ko";
+import format from "date-fns/format";
 
+class koLocalizedUtils extends DateFnsUtils {
+  getCalendarHeaderText(date) {
+    return format(date, "yyyy년　　 MM월", { locale: this.locale });
+  }
+}
 const HRorganization = () => {
   const [startDate, setStartDate] = useState(new Date());
   const [endDate, setEndDate] = useState(new Date());
+  
+  const [selectDepart, setSelectDepart] = useState()
+
+  const [textName, setTextName]= useState();
+
+  const handleStartDateChange = (date) => {
+    setStartDate(getFormatDate(date));
+  };
+
+  const handleEndDatetDateChange = (date) => {
+    setEndDate(getFormatDate(date));
+  };
+
+
+  function getFormatDate(date) {
+    var year = date.getFullYear(); //yyyy
+    var month = 1 + date.getMonth(); //M
+    month = month >= 10 ? month : "0" + month; //month 두자리로 저장
+    var day = date.getDate(); //d
+    day = day >= 10 ? day : "0" + day; //day 두자리로 저장
+    return year + "-" + month + "-" + day;
+  }
+
+  const handleSelectDepart = (event) => {
+    setSelectDepart(event.target.value);
+  };
+
+
+  const textNameHandle = (e) => {
+    setTextName(e.target.value);
+  }
+
+
+
+
+
+
 
   return (
-    <div className="HRorganization_main">
-      <div className="HRorganization_dateBox">
-        <span>기준일자</span>
+    <>
+      <div className="HRorganization_main">
+        <div className="HRorganization_dateBox">
+          <span>기준일자</span>
 
-        <DatePicker
-          locale={ko}
-          dateFormat="yyyy-MM-dd"
-          selected={startDate}
-          onChange={(date) => setStartDate(date)}
-          selectsStart
-          startDate={startDate}
-          endDate={endDate}
-        />
+          <MuiPickersUtilsProvider utils={koLocalizedUtils} locale={koLocale}>
+            <DatePicker
+              autoOk
+              variant="inline"
+              views={["year", "month", "date"]}
+              format="yyyy-MM-dd"
+              value={startDate}
+              inputVariant="outlined"
+              showTodayButton
+              className="startDate"
+              size="small"
+              todayLabel="오늘"
+              onChange={handleStartDateChange}
+            />
+          </MuiPickersUtilsProvider>
+          <div className="HRorganization_bar"></div>
+
+          
+          <MuiPickersUtilsProvider utils={koLocalizedUtils} locale={koLocale}>
+          <DatePicker
+              autoOk
+              variant="inline"
+              views={["year", "month", "date"]}
+              format="yyyy-MM-dd"
+              value={endDate}
+              inputVariant="outlined"
+              className="startDate"
+              showTodayButton
+              size="small"
+              todayLabel="오늘"
+              onChange={handleEndDatetDateChange}
+            />
+            </MuiPickersUtilsProvider>
+       
+            <FormControl>
+              <Select
+                value = {selectDepart || ""}
+                sx={{
+                  minWidth: "153px",
+                  height: 39,
+                  marginLeft: "15px",
+                  marginRight: "26px",
+                }}
+                onChange={handleSelectDepart}
+              >
+                <MenuItem sx={{ minWidth: "153px", height: 30 }} value={"전체부서"}>
+                  전체부서
+                </MenuItem>
+              </Select>
+            </FormControl>
+            <FormControl>
+              <TextField
+                id="outlined-basic"
+                label="사번/성명"
+                variant="outlined"
+                size="small"
+                onChange={textNameHandle}
+              />
+            </FormControl>
+            <button className="HRorganization_btn">검색</button>
+        </div>
+        <HrInfoTable startDate={startDate} endDate = {endDate} selectDepart = {selectDepart} textName = {textName}/>
       </div>
-    </div>
+    </>
   );
 };
 
---
+++ capston/src/HRorganization/HrInfoTable.js
@@ -0,0 +1,72 @@
+import React, { useEffect, useState } from 'react';
+import axios from "axios";
+const HrInfoTable = ({startDate, endDate, selectDepart, textName}) => {
+    const [peopleData, setPeopleData] = useState();
+
+ 
+
+    useEffect(()=> {
+        axios.post("http://43.200.115.198:8080/empselect.jsp").then((res)=>{
+            setPeopleData(res.data.ITEMS);            
+        }).catch((Error)=> {
+            console.log(Error);
+        })
+
+    }, [])
+
+    console.log(peopleData);
+
+    return (
+        <div className='HrInfoTable'>
+            <p>사원정보현황</p>
+
+            <div className='info_Table'>
+                {!peopleData ? ("No data found") : (
+                    <table>
+                        <thead>
+                            <tr>
+                                <th>번호</th>
+                                <th>사진</th>
+                                <th>사번</th>
+                                <th>성명</th>
+                                <th>영문성명</th>
+                                <th>주민번호</th>
+                                <th>성별</th>
+                                <th>부서명</th>
+                                <th>직책</th>
+                                <th>입사일</th>
+                                <th>퇴사일</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                           {peopleData.map(function(name, index){
+                                return(
+                                    <>
+                                        <tr>
+                                            <td>{index + 1}</td>
+                                            <td>사진</td>
+                                            <td>{name.sabun}</td>
+                                            <td>{name.name}</td>
+                                            <td>{name.eng_name}</td>
+                                            <td>{name.identity}</td>
+                                            <td>{name.gender === "0" ? "남자" : "여자"}</td>
+                                            <td>{name.dept === "01" ? "경영지원부" : "" || name.dept === "02" ? "경영관리" : "" || name.dept === "03" ? "침해대응부" : "" || name.dept === "04" ? "관제센터" : "" || name.dept === "05" ? "보안연구부" : "" || name.dept === "06" ? "보안취약점연구부" : ""}</td>
+                                            <td>{name.rank === "1" ? "사원" : "" || name.rank === "2" ? "대리" : "" || name.rank === "3" ? "과장" : "" || name.rank === "4" ? "차장" : "" || name.rank === "5" ? "부장" : "" || name.rank === "6" ? "이사" : "" || name.rank === "7" ? "상무" : ""  }</td>
+                                            <td>{name.start_date}</td>
+                                            <td>{name.retire_date === null ? "-":""}</td>
+
+                                      
+                                        </tr>
+                                        </>
+                                )
+                           })}
+                        </tbody>
+                    </table>
+                )
+}
+            </div>
+        </div>
+    );
+};
+
+export default HrInfoTable;
