471bd2f2bbbf24bb3e7671249cf01c7c9a1464ae
park <pjh971212@gmail.com>
2022-10-18 21:44:22 +0900

    조직관리 마무리

--- capston/src/App.js
+++ capston/src/App.js
@@ -1,29 +1,28 @@
-import React from 'react';
+import React from "react";
 import Login from "./login";
 import Empinfo from "./empinfo";
 import Findpw from "./login/findpw";
 import MainNav from "./mainNav";
+import "jqwidgets-scripts/jqwidgets/styles/jqx.base.css";
+//import "jqwidgets-scripts/jqwidgets/styles/jqx.material-purple.css";
 
-import { BrowserRouter, Route, Router, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Router, Routes } from "react-router-dom";
 
 const App = () => {
-	return (
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
 
-		<BrowserRouter>
-			<Routes>
-			
-				<Route path="/" element={<Login/>}/>
+        {/* 비밀번호 찾기 */}
+        <Route path="/findpw/" element={<Findpw />} />
 
-				{/* 비밀번호 찾기 */ }
-				<Route path="/findpw/" element={<Findpw/>}/>
-
-				{/* 네비게이션으로 이동 */}
-				<Route path="/mainNav/*" element={<MainNav/>}/>
-				
-			</Routes>
-		</BrowserRouter>
-		//<Empinfo />
-	);
+        {/* 네비게이션으로 이동 */}
+        <Route path="/mainNav/*" element={<MainNav />} />
+      </Routes>
+    </BrowserRouter>
+    //<Empinfo />
+  );
 };
 
 export default App;
