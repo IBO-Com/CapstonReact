2eb738a62870d58e6ce4558bd6fb72d2ae5e07e3
iam6ukk <kyk001020@naver.com>
2022-10-18 14:06:06 +0900

    발령 모달창 구현 중

---
+++ capston/src/modals/AppointmentModal.js
@@ -0,0 +1,19 @@
+import React from "react";
+import Modal from "@material-ui/core/Modal";
+
+const AppointmentModal = ({ openModal, setOpenModal }) => {
+  return (
+    <Modal>
+      <div className="AppointmentModal_container">
+        <div className="AppointmentModal_title">
+          <p>인사발령</p>
+        </div>
+        <div className="AppointmentModal_content"></div>
+        <button onClick={() => setOpenModal(false)}>취소</button>
+        <button>등록</button>
+      </div>
+    </Modal>
+  );
+};
+
+export default AppointmentModal;
