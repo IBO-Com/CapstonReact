2e4d21021485e60a94ecb6f825e123685507b2fb
DESKTOP-5SPU3RG\4stec <siyeon00@kakao.com>
2022-10-21 11:34:31 +0900

    인사기록카드 인쇄 폼 구현 완료

--- capston/src/empCard/formCareercert.js
+++ capston/src/empCard/formCareercert.js
@@ -3,8 +3,60 @@ import "../css/personnelcard/formCareeCert.css";
 
 const App = ({componentRef}) => {
    return (
-      <div ref={componentRef}>
-         <p>경력증명서 페이지입니다</p>
+      <div ref={componentRef} className="formProofofempMain">
+          <div className="perCardtitle">
+        <p>경력증명서</p>
+        </div>
+        <table className="formProofofeTable">
+            <tr className="formProofofeFirst">
+               <td>성명</td>
+               <td></td>
+               <td>생년월일</td>
+               <td></td>
+            </tr>
+
+            <tr className="formProofofeFirst">
+               <td>주소</td>
+               <td colSpan={3}></td>
+            </tr>
+
+            <tr className="formProofofeFirst">
+               <td>연락처</td>
+               <td></td>
+               <td>부서</td>
+               <td></td>
+            </tr>
+
+            <tr className="formProofofeFirst">
+               <td>입사일자</td>
+               <td></td>
+               <td>직책</td>
+               <td></td>
+            </tr>
+            <tr className="formProofofeFirst formProofofeSecond">
+               <td>재직기간</td>
+               <td colSpan={3} className="enterForm">
+                  <div className='date_form'>
+                     <div className='date'>
+                        <p>2020년 1월 1일부터</p>
+                        <p>2020년 12월 31일까지</p>
+                     </div>
+                     <div className='date2'>
+                        <p>(1년 0개월 0일)</p>
+                     </div>
+                  </div>
+
+               </td>
+            </tr>
+            
+        </table>
+
+         <div className="footer_info_form">
+            <p>상기와 같이 재직하고 있음을 증명합니다.</p>   
+            <p>2022년 10월 9일</p>
+            <p className="mbt">IBO</p>
+            <p>대표이사 담 당 자 (인)</p>
+         </div>   
       </div>
    );
 };
--- capston/src/empCard/formProofofemp.js
+++ capston/src/empCard/formProofofemp.js
@@ -1,12 +1,53 @@
-import React from 'react';
+import React from "react";
 import "../css/personnelcard/formProofEmp.css";
 
-const App = ({componentRef}) => {
-   return (
-      <div ref={componentRef}>
-         <p>재직증명서 페이지입니다</p>
-      </div>
-   );
+const App = ({ componentRef }) => {
+  return (
+    <div ref={componentRef} className="formProofofempMain">
+      <div className="perCardtitle">
+        <p>재직증명서</p>
+        </div>
+        <table className="formProofofeTable">
+            <tr className="formProofofeFirst">
+               <td>성명</td>
+               <td></td>
+               <td>생년월일</td>
+               <td></td>
+            </tr>
+
+            <tr className="formProofofeFirst">
+               <td>주소</td>
+               <td colSpan={3}></td>
+            </tr>
+
+            <tr className="formProofofeFirst">
+               <td>연락처</td>
+               <td></td>
+               <td>부서</td>
+               <td></td>
+            </tr>
+
+            <tr className="formProofofeFirst">
+               <td>입사일자</td>
+               <td></td>
+               <td>직책</td>
+               <td></td>
+            </tr>
+            <tr className="formProofofeFirst formProofofeSecond">
+               <td>용도</td>
+               <td colSpan={3}>제출용</td>
+            </tr>
+            
+        </table>
+
+         <div className="footer_info_form">
+            <p>상기와 같이 재직하고 있음을 증명합니다.</p>   
+            <p>2022년 10월 9일</p>
+            <p className="mbt">IBO</p>
+            <p>대표이사 담 당 자 (인)</p>
+         </div>   
+    </div>
+  );
 };
 
 export default App;
