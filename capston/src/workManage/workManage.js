875fdffbdc8a5a84209e2824ed1ca577111870b2
Kimjinwon <59737252+hdev1004@users.noreply.github.com>
2022-10-24 22:25:02 +0900

    css 높이 수정 	modified:   capston/src/css/HRorganization/HRorganization.css 	modified:   capston/src/css/personnelcard/PersonnelCard.css 	modified:   capston/src/css/vactionManage/vactionManage.css 	new file:   capston/src/css/workManage/workManage.css 	modified:   capston/src/workManage/workManage.js

--- capston/src/workManage/workManage.js
+++ capston/src/workManage/workManage.js
@@ -1,8 +1,9 @@
 import React from "react";
+import "../css/workManage/workManage.css";
 
 const App = () => {
     return (
-        <div>
+        <div className="workManage_App">
             이것은 근무 관리 입니다.
         </div>
     )
