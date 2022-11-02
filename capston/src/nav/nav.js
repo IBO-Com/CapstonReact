65a45fd53be118f49bb0c4e32e4d6ef774396855
park <pjh971212@gmail.com>
2022-11-02 11:29:05 +0900

    권한관련 commit

--- capston/src/nav/nav.js
+++ capston/src/nav/nav.js
@@ -6,6 +6,7 @@ import Calendar from "../img/calendar.png";
 import Paid from "../img/paid.png";
 import Leave from "../img/leave.png";
 import { useState } from "react";
+import * as Cookie from "../cookies/cookies";
 
 function App({ currentClick, setCurrentClick }) {
   const [selected, setSelected] = useState(null);
@@ -25,6 +26,7 @@ function App({ currentClick, setCurrentClick }) {
     <div className="nav_wrapper">
       <div className="nav_block" />
       <div className="nav_accordion">
+        {console.log(Cookie.getCookie("loginInfo").authority)}
         {Object.keys(nav).map((item, i) => (
           <div className={selected == i ? "nav_item_show" : "nav_item"}>
             <div className="nav_title" onClick={() => toggle(i)}>
@@ -56,57 +58,112 @@ function App({ currentClick, setCurrentClick }) {
   );
 }
 
-const nav = {
-  기본정보: {
-    menu: ["공통코드정보", "조직도"],
-    link: ["/mainNav/a1", "/mainNav/a2"],
-  },
-  "인사/조직관리": {
-    menu: [
-      "인사정보",
-      "인사정보등록",
-      "인사/정보관리",
-      "조직관리",
-      "인사기록카드",
-      "사원정보현황",
-      "인사명령",
-    ],
-    link: [
-      "/mainNav/empinfo",
-      "/mainNav/empregister",
-      "/mainNav/b3",
-      "/mainNav/setorganize",
-      "/mainNav/personnelcard",
-      "/mainNav/hrorganization",
-      "/mainNav/personnelappointment",
-    ],
-  },
-  근태관리: {
-    menu: ["근태등록", "근무관리", "휴가관리", "근태/근무현황"],
-    link: [
-      "/mainNav/attendanceregister",
-      "/mainNav/workManage",
-      "/mainNav/vacationManage",
-      "/mainNav/attendancestatus",
-    ],
-  },
-  급여관리: {
-    menu: ["급여기본관리", "개인급여관리", "월급여조회", "급여명세서조회"],
-    link: [
-      "/mainNav/d1",
-      "/mainNav/d2",
-      "/mainNav/monthlySalaryCheck",
-      "/mainNav/payslipCheck",
-    ],
-  },
-  퇴직관리: {
-    menu: ["퇴직승인", "퇴직금산정", "퇴직금명세서"],
-    link: ["/mainNav/retirementapproval", "/mainNav/e2", "/mainNav/severancepay"],
-  },
-  테스트: {
-    menu: ["발령조회", "테스트", "page 3"],
-    link: ["/mainNav/appointmentcheck", "/mainNav/Test", "/mainNav/test3"],
-  },
-};
+const nav =
+  Cookie.getCookie("loginInfo").authority === "0"
+    ? {
+        기본정보: {
+          menu: ["공통코드관리", "조직도"],
+          link: ["/mainNav/setcommoncode", "/mainNav/a2"],
+        },
+        "인사/조직관리": {
+          menu: [
+            "인사정보",
+            "인사정보등록",
+            "인사/정보관리",
+            "조직관리",
+            "인사기록카드",
+            "사원정보현황",
+            "인사명령",
+          ],
+          link: [
+            "/mainNav/empinfo",
+            "/mainNav/empregister",
+            "/mainNav/b3",
+            "/mainNav/setorganize",
+            "/mainNav/personnelcard",
+            "/mainNav/hrorganization",
+            "/mainNav/personnelappointment",
+          ],
+        },
+        근태관리: {
+          menu: ["근태등록", "근무관리", "휴가관리", "근태/근무현황"],
+          link: [
+            "/mainNav/attendanceregister",
+            "/mainNav/workManage",
+            "/mainNav/vacationManage",
+            "/mainNav/attendancestatus",
+          ],
+        },
+        급여관리: {
+          menu: [
+            "급여기본관리",
+            "개인급여관리",
+            "월급여조회",
+            "급여명세서조회",
+          ],
+          link: [
+            "/mainNav/d1",
+            "/mainNav/d2",
+            "/mainNav/monthlySalaryCheck",
+            "/mainNav/payslipCheck",
+          ],
+        },
+        퇴직관리: {
+          menu: ["퇴직승인", "퇴직금산정", "퇴직금명세서"],
+          link: [
+            "/mainNav/retirementapproval",
+            "/mainNav/e2",
+            "/mainNav/severancepay",
+          ],
+        },
+        테스트: {
+          menu: ["발령조회", "테스트", "page 3"],
+          link: [
+            "/mainNav/appointmentcheck",
+            "/mainNav/Test",
+            "/mainNav/test3",
+          ],
+        },
+      }
+    : {
+        기본정보: {
+          menu: ["공통코드관리", "조직도"],
+          link: ["/mainNav/setcommoncode", "/mainNav/a2"],
+        },
+        "인사/조직관리": {
+          menu: ["인사정보", "인사/정보관리", "인사기록카드"],
+          link: [
+            "/mainNav/empinfo",
+            "/mainNav/hrorganization",
+            "/mainNav/personnelcard",
+          ],
+        },
+        근태관리: {
+          menu: ["휴가관리", "근태/근무현황"],
+          link: ["/mainNav/vacationManage", "/mainNav/attendancestatus"],
+        },
+        급여관리: {
+          menu: [
+            "급여기본관리",
+            "개인급여관리",
+            "월급여조회",
+            "급여명세서조회",
+          ],
+          link: [
+            "/mainNav/d1",
+            "/mainNav/d2",
+            "/mainNav/monthlySalaryCheck",
+            "/mainNav/payslipCheck",
+          ],
+        },
+        퇴직관리: {
+          menu: ["퇴직승인", "퇴직금산정", "퇴직금명세서"],
+          link: [
+            "/mainNav/retirementapproval",
+            "/mainNav/e2",
+            "/mainNav/severancepay",
+          ],
+        },
+      };
 
 export default App;
