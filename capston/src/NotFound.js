5cd35a491e0ee3dcee828269899c56af8cf7bbd3
Kimjinwon <59737252+hdev1004@users.noreply.github.com>
2022-11-07 22:09:56 +0900

    not found 수정 	modified:   capston/src/NotFound.js 	new file:   capston/src/css/notFound.css

--- capston/src/NotFound.js
+++ capston/src/NotFound.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import "./css/notFound.css";
 
 const NotFound = () => {
     return (
-        <div>
-            404 Error
+        <div className='notFound_App'>
+            <div className='notFound_div'>
+                <span className='notFound_icon'>😭</span>
+                <span className='notFound_text'>앗, 페이지가 없어요.</span>
+            </div>  
         </div>
     );
 };
