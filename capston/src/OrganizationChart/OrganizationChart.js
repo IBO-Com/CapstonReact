ce44c365d57239ba9aa9dae0dea0f304776efc50
kimzzaelim <kimchaelim38@gmail.com>
2022-11-14 23:14:17 +0900

    조직도 초안

---
+++ capston/src/OrganizationChart/OrganizationChart.js
@@ -0,0 +1,31 @@
+import React from "react";
+import "../css/CommonCode/CommonCode.css";
+//import organizationchart from "../src/img/organizationchart.png";
+import organizationchart from "../img/organizationchart.png"
+const App = () => {
+    return (
+    <div className="CommonCode_App">
+        <div className="OrganizationChart_container">
+        <div
+          style={{
+                  padding: "15px",
+                  fontSize: "20px",
+                  fontWeight: "bold",
+                  }}
+                >
+            <h4>조직도</h4>
+              <div className="OrganizationChart_Img">
+                  <img className="organizationchart" src={organizationchart} alt={"사진"} />
+                  <div className="OrganizationChart_Description_Container">
+                  <h4>조직도 소개</h4>
+                  <div className="OrganizationChart_Description"></div>
+                </div>
+              </div>
+            </div>
+            </div>
+            </div>
+       
+    )
+}
+
+export default App;
