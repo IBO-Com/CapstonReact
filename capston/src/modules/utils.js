4dc24feafd4a4f624894dbaca1c36b28ac37d71f
hdev1004 <h.dev@kakao.com>
2022-11-16 15:59:59 +0900

    유틸 수정 	modified:   capston/src/modules/utils.js

--- capston/src/modules/utils.js
+++ capston/src/modules/utils.js
@@ -2,6 +2,10 @@ export const AttendanceRegisterToPay = (data) => {
     let userData = {};
     let keys = Object.keys(data);
     for(let i = 0; i < keys.length; i ++) {
-        console.log(data[i]);
+        let eachData = data[i];
+
+        let normal_datetime = eachData.normal_datetime.trim() === "" ? 0 : parseInt(eachData.normal_datetime);
+        
     }
+    console.log(userData);
 }
