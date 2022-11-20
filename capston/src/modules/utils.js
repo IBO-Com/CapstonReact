f9dfbb463b00ed178ee41281409cfb1191bee7fc
Kimjinwon <59737252+hdev1004@users.noreply.github.com>
2022-11-21 02:29:52 +0900

    유틸 수정 	modified:   capston/src/modules/utils.js

--- capston/src/modules/utils.js
+++ capston/src/modules/utils.js
@@ -1,77 +1,74 @@
 import qs from "qs";
 import axios from "axios";
 
-export const AttendanceRegisterToPay = (data) => {
-  console.log(data);
-  let userData = {};
-  let keys = Object.keys(data);
-  let normalSum = 0;
-  for (let i = 0; i < keys.length; i++) {
-    let eachData = data[i];
+export const AttendanceRegisterToPay = () => {
+  axios.post("http://43.200.115.198:8080/getAttendanceMonth.jsp").then((res)=> {
+    let data = res.data.ITEMS;
+    let userData = {};
+    let keys = Object.keys(data);
+    let normalSum = 0;
+    console.log(data);
+    for (let i = 0; i < keys.length; i++) {
+      let eachData = data[i];
 
-    let normal_datetime =
-      eachData.normal_datetime === "" ? 0 : parseInt(eachData.normal_datetime);
-    let over_datetime =
-      eachData.over_datetime === "" ? 0 : parseInt(eachData.over_datetime);
-    let night_datetime =
-      eachData.night_datetime === "" ? 0 : parseInt(eachData.night_datetime);
-    let holiday_datetime =
-      eachData.holiday_datetime === ""
-        ? 0
-        : parseInt(eachData.holiday_datetime);
-    let total_datetime =
-      normal_datetime + over_datetime + night_datetime + holiday_datetime;
-    if (eachData.sabun == "1012210000") {
-      normalSum += normal_datetime;
-    }
-    //console.log(eachData.name, normal_datetime, over_datetime, night_datetime, holiday_datetime)
-    if (userData[eachData.sabun]) {
-      //데이터 있을 때
-      userData[eachData.sabun].cnt += 1;
-      userData[eachData.sabun].normal_datetime += normal_datetime;
-      userData[eachData.sabun].over_datetime += over_datetime;
-      userData[eachData.sabun].night_datetime += night_datetime;
-      userData[eachData.sabun].holiday_datetime += holiday_datetime;
-      userData[eachData.sabun].total_datetime += total_datetime;
-    } else {
-      //데이터 없을 때
+      let normal_datetime =
+        eachData.normal_datetime === "" ? 0 : parseInt(eachData.normal_datetime);
+      let over_datetime =
+        eachData.over_datetime === "" ? 0 : parseInt(eachData.over_datetime);
+      let night_datetime =
+        eachData.night_datetime === "" ? 0 : parseInt(eachData.night_datetime);
+      let holiday_datetime =
+        eachData.holiday_datetime === ""
+          ? 0
+          : parseInt(eachData.holiday_datetime);
+      let total_datetime =
+        normal_datetime + over_datetime + night_datetime + holiday_datetime;
+      
+      //console.log(eachData.name, normal_datetime, over_datetime, night_datetime, holiday_datetime)
       userData[eachData.sabun] = {
-        cnt: 1,
-        indate:
-          eachData.in_date.slice(0, 4) + eachData.in_date.slice(5, 7) + "01",
-        sabun: eachData.sabun,
-        name: eachData.name,
-        normal_datetime: normal_datetime,
-        over_datetime: over_datetime,
-        night_datetime: night_datetime,
-        holiday_datetime: holiday_datetime,
-        total_datetime: total_datetime,
-      };
+        ...userData[eachData.sabun],
+        [eachData.in_date.slice(0, 6)]: {
+          normal_datetime: normal_datetime,
+          over_datetime: over_datetime,
+          night_datetime: night_datetime,
+          total_datetime: total_datetime,
+          holiday_datetime: holiday_datetime,
+          sabun: eachData.sabun,
+          indate: eachData.in_date,
+          day: eachData.day
+        }
+      }
+      
+      
     }
-  }
 
-  let postParam = {
-    data: JSON.stringify(userData),
-    length: Object.keys(userData).length,
-  };
-  console.log(userData);
+    let postParam = {
+      data: JSON.stringify(userData),
+      length: Object.keys(userData).length,
+    };
+    console.log("getAttend Data : ", userData);
 
-  postParam = qs.stringify(postParam);
-  
-  //http://localhost:8080/CapstonProject/AttendanceRegisterToPay.jsp
-  //http://43.200.115.198:8080/AttendanceRegisterToPay.jsp
-  axios
-    .post(
-      "http://43.200.115.198:8080/AttendanceRegisterToPay.jsp",
-      postParam
-    )
-    .then((res) => {
-      console.log(res);
-    })
-    .catch((Error) => {
+    postParam = qs.stringify(postParam);
+    
+    //http://localhost:8080/CapstonProject/AttendanceRegisterToPay.jsp
+    //http://43.200.115.198:8080/AttendanceRegisterToPay.jsp
+    
+    axios
+      .post(
+        "http://43.200.115.198:8080/AttendanceRegisterToPay.jsp",
+        postParam
+      )
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((Error) => {
+        console.log(Error);
+      });
+    console.log(userData);
+    
+    }).catch((Error) => {
       console.log(Error);
-    });
-  console.log(userData);
+    })
   
 };
 
