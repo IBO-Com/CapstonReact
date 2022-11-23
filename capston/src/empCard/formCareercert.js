498425c6943a3eaec54b6fc10b31d9634d1b4e3c
iam6ukk <kyk001020@naver.com>
2022-11-23 11:19:48 +0900

    증명서 폼 수정완료

--- capston/src/empCard/formCareercert.js
+++ capston/src/empCard/formCareercert.js
@@ -7,137 +7,201 @@ import qs from "qs";
 import IBOstamp from "../img/stamp.png";
 
 const App = ({ componentRef, sabun }) => {
-   const todayTime = () => {
-      let now = new Date();
-      let todayYear = now.getFullYear();
-      let todayMonth = now.getMonth() + 1;
-      let toDayDate = now.getDate();
+  const todayTime = () => {
+    let now = new Date();
+    let todayYear = now.getFullYear();
+    let todayMonth = now.getMonth() + 1;
+    let toDayDate = now.getDate();
 
-      return todayYear + "-" + todayMonth + "-" + toDayDate;
-   }
+    return todayYear + "-" + todayMonth + "-" + toDayDate;
+  };
 
-   const [today, setToday] = useState(new Date());
-   const [endDate, setEndDate] = useState(new Date());
-   const [defaultYear, setDefaultYear] = useState("19");
-   const [userData, setUserData] = useState();
-   const [dept, setDept] = useState("");
-   const [team, setTeam] = useState("");
-   const [rank, setRank] = useState("");
-   const [center, setCenter] = useState("");
+  const [today, setToday] = useState(new Date());
+  const [endDate, setEndDate] = useState(new Date());
+  const [defaultYear, setDefaultYear] = useState("19");
+  const [userData, setUserData] = useState();
+  const [dept, setDept] = useState("");
+  const [team, setTeam] = useState("");
+  const [rank, setRank] = useState("");
+  const [center, setCenter] = useState("");
 
-   const [workYear, setWorkYear] = useState("0");
-   const [workMonth, setWorkMonth] = useState("0");
-   const [workDay, setWorkDay] = useState("0");
+  const [workYear, setWorkYear] = useState("0");
+  const [workMonth, setWorkMonth] = useState("0");
+  const [workDay, setWorkDay] = useState("0");
 
+  useEffect(() => {
+    if (!sabun) return;
+    let postParam = qs.stringify({
+      sabunOrName: sabun,
+    });
 
-   useEffect(() => {
-      if (!sabun) return;
-      let postParam = qs.stringify({
-         sabunOrName: sabun
+    axios
+      .post("http://43.200.115.198:8080/empselect.jsp", postParam)
+      .then((response) => {
+        setUserData(response.data.ITEMS[0]);
+        let userInfo = response.data.ITEMS[0];
+
+        if (
+          today.getFullYear() - 2000 <
+          parseInt(userInfo["identity"].slice(0, 2))
+        ) {
+          setDefaultYear("19");
+        } else {
+          setDefaultYear("20");
+        }
+
+        let startDate = new Date(
+          userInfo["start_date"].slice(0, 4),
+          parseInt(userInfo["start_date"].slice(4, 6)) - 1, //Date 연산으로 인한 -1을 해주어야 함
+          userInfo["start_date"].slice(6, 8)
+        );
+
+        if (userInfo["retire_cls"] == 1) {
+          //퇴직일경우 EndDate를 퇴직일자로 변경
+          setEndDate(
+            new Date(
+              userInfo["retire_date"].slice(0, 4),
+              parseInt(userInfo["retire_date"].slice(4, 6)) - 1, //Date 연산으로 인한 -1을 해주어야 함
+              userInfo["retire_date"].slice(6, 8)
+            )
+          );
+        }
+
+        GetYearOfWork.getYearOfWork(
+          startDate,
+          endDate,
+          setWorkYear,
+          setWorkMonth,
+          setWorkDay
+        );
+
+        GetCDTR.getCDTR(
+          userInfo["center"],
+          userInfo["dept"],
+          userInfo["team"],
+          userInfo["rank"],
+          setCenter,
+          setDept,
+          setTeam,
+          setRank
+        );
       });
+  }, [sabun]);
 
-      axios.post("http://43.200.115.198:8080/empselect.jsp", postParam).then((response) => {
-         setUserData(response.data.ITEMS[0]);
-         let userInfo = response.data.ITEMS[0];
-
-         if (
-            today.getFullYear() - 2000 <
-            parseInt(userInfo["identity"].slice(0, 2))
-         ) {
-            setDefaultYear("19");
-         } else {
-            setDefaultYear("20");
-         }
-
-         let startDate = new Date(
-            userInfo["start_date"].slice(0, 4),
-            parseInt(userInfo["start_date"].slice(4, 6)) - 1, //Date 연산으로 인한 -1을 해주어야 함
-            userInfo["start_date"].slice(6, 8)
-         );
-
-         if (userInfo["retire_cls"] == 1) { //퇴직일경우 EndDate를 퇴직일자로 변경
-            setEndDate(new Date(
-               userInfo["retire_date"].slice(0, 4),
-               parseInt(userInfo["retire_date"].slice(4, 6)) - 1, //Date 연산으로 인한 -1을 해주어야 함
-               userInfo["retire_date"].slice(6, 8)
-            ));
-         }
-
-         GetYearOfWork.getYearOfWork(startDate, endDate, setWorkYear, setWorkMonth, setWorkDay);
-
-         GetCDTR.getCDTR(userInfo["center"], userInfo["dept"], userInfo["team"], userInfo["rank"],
-            setCenter, setDept, setTeam, setRank);
-      });
-   }, [sabun])
-
-   return (
-      <div ref={componentRef} className="formProofofempMain">
-         <div className="perCardtitle">
-            <p>경력증명서</p>
-         </div>
-         <table className="formProofofeTable">
-            <tr className="formProofofeFirst">
-               <td>성명</td>
-               <td>{userData ? userData["name"] : ""}</td>
-               <td>생년월일</td>
-               <td>{userData ? defaultYear + userData["identity"].slice(0, 2) + "-" + userData["identity"].slice(2, 4) + "-" + userData["identity"].slice(4, 6) : ""}</td>
-            </tr>
-
-            <tr className="formProofofeFirst">
-               <td>주소</td>
-               <td colSpan={3}>{userData ? userData["address"] : ""} {userData ? userData["address_detail"] : ""}</td>
-            </tr>
-
-            <tr className="formProofofeFirst">
-               <td>연락처</td>
-               <td>{userData ? userData["tel_no"].slice(0, 3) + "-" + userData["tel_no"].slice(3, 7) + "-" + userData["tel_no"].slice(7, 11) : ""}</td>
-               <td>부서</td>
-               <td>{dept}</td>
-            </tr>
-
-            <tr className="formProofofeFirst">
-               <td>입사일자</td>
-               <td>{userData ? userData["start_date"].slice(0, 4) + "-" + userData["start_date"].slice(4, 6) + "-" + userData["start_date"].slice(6, 8) : ""}</td>
-               <td>직책</td>
-               <td>{rank}</td>
-            </tr>
-            <tr className="formProofofeFirst formProofofeSecond">
-               <td className="formProofofe_wid">재직기간</td>
-               <td colSpan={3} className="enterForm">
-                  <div className='date_form'>
-                     <div className='date'>
-                        <p>{workYear}년 {workMonth}개월 {workDay}일</p>
-                     </div>
-
-                     <div className='date2'>
-                        <p>{userData ? userData["start_date"].slice(0, 4) + "년 " + userData["start_date"].slice(4, 6) + "월 " + userData["start_date"].slice(6, 8) + "일 부터" : ""}</p>
-                        <p>
-                           {
-                              userData ? (
-                                 <p>{endDate.getFullYear() + "년 " + (endDate.getMonth() + 1) + "월 " + endDate.getDate() + "일 까지"}</p>
-                              ) : (
-                                 <></>
-                              )
-                           }
-                        </p>
-                     </div>
-                  </div>
-               </td>
-            </tr>
-
-         </table>
-
-         <div className="footer_info_form">
-            <p>상기와 같이 경력을 증명합니다.</p>
-            <p>{todayTime().slice(0, 4)}년 {todayTime().slice(5, 7)}월 {todayTime().slice(8, 10)}일</p>
-            <p className="mbt">IBO</p>
-            <p>대표이사 담 당 자 &nbsp;&nbsp; (인)</p>
-            <div className="formProofDiv">
-               <img className="formProof" src={IBOstamp} alt="직인" />
-            </div>
-         </div>
+  return (
+    <div ref={componentRef} className="formProofofempMain">
+      <div className="perCardtitle">
+        <p>경력증명서</p>
       </div>
-   );
+      <table className="formProofofeTable">
+        <tr className="formProofofeFirst">
+          <td>성명</td>
+          <td>{userData ? userData["name"] : ""}</td>
+          <td>생년월일</td>
+          <td>
+            {userData
+              ? defaultYear +
+                userData["identity"].slice(0, 2) +
+                "-" +
+                userData["identity"].slice(2, 4) +
+                "-" +
+                userData["identity"].slice(4, 6)
+              : ""}
+          </td>
+        </tr>
+
+        <tr className="formProofofeFirst">
+          <td>주소</td>
+          <td colSpan={3}>
+            {userData ? userData["address"] : ""}{" "}
+            {userData ? userData["address_detail"] : ""}
+          </td>
+        </tr>
+
+        <tr className="formProofofeFirst">
+          <td>입사일자</td>
+          <td>
+            {userData
+              ? userData["start_date"].slice(0, 4) +
+                "-" +
+                userData["start_date"].slice(4, 6) +
+                "-" +
+                userData["start_date"].slice(6, 8)
+              : ""}
+          </td>
+          <td>부서</td>
+          <td>{dept}</td>
+        </tr>
+
+        <tr className="formProofofeFirst">
+          <td>퇴사일자</td>
+          <td>
+            {userData
+              ? userData["retire_date"].slice(0, 4) +
+                "-" +
+                userData["retire_date"].slice(4, 6) +
+                "-" +
+                userData["retire_date"].slice(6, 8)
+              : ""}
+          </td>
+          <td>직책</td>
+          <td>{rank}</td>
+        </tr>
+        <tr className="formProofofeFirst formProofofeSecond">
+          <td className="formProofofe_wid">재직기간</td>
+          <td colSpan={3} className="enterForm">
+            <div className="date_form">
+              <div className="date">
+                <p>
+                  {workYear}년 {workMonth}개월 {workDay}일
+                </p>
+              </div>
+
+              <div className="date2">
+                <p>
+                  {userData
+                    ? userData["start_date"].slice(0, 4) +
+                      "년 " +
+                      userData["start_date"].slice(4, 6) +
+                      "월 " +
+                      userData["start_date"].slice(6, 8) +
+                      "일 부터"
+                    : ""}
+                </p>
+                <p>
+                  {userData ? (
+                    <p>
+                      {endDate.getFullYear() +
+                        "년 " +
+                        (endDate.getMonth() + 1) +
+                        "월 " +
+                        endDate.getDate() +
+                        "일 까지"}
+                    </p>
+                  ) : (
+                    <></>
+                  )}
+                </p>
+              </div>
+            </div>
+          </td>
+        </tr>
+      </table>
+
+      <div className="footer_info_form">
+        <p>상기와 같이 경력을 증명합니다.</p>
+        <p>
+          {todayTime().slice(0, 4)}년 {todayTime().slice(5, 7)}월{" "}
+          {todayTime().slice(8, 10)}일
+        </p>
+        <p className="mbt">IBO</p>
+        <p>대표이사 담 당 자 &nbsp;&nbsp; (인)</p>
+        <div className="formProofDiv">
+          <img className="formProof" src={IBOstamp} alt="직인" />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default App;
--- capston/src/empCard/formProofofemp.js
+++ capston/src/empCard/formProofofemp.js
@@ -6,95 +6,126 @@ import qs from "qs";
 import IBOstamp from "../img/stamp.png";
 
 const App = ({ componentRef, sabun }) => {
-   const todayTime = () => {
-      let now = new Date();
-      let todayYear = now.getFullYear();
-      let todayMonth = now.getMonth() + 1;
-      let toDayDate = now.getDate();
+  const todayTime = () => {
+    let now = new Date();
+    let todayYear = now.getFullYear();
+    let todayMonth = now.getMonth() + 1;
+    let toDayDate = now.getDate();
 
-      return todayYear + "-" + todayMonth + "-" + toDayDate;
-  }
+    return todayYear + "-" + todayMonth + "-" + toDayDate;
+  };
 
-   const [today, setToday] = useState(new Date());
-   const [defaultYear, setDefaultYear] = useState("19");
-   const [userData, setUserData] = useState();
-   const [dept, setDept] = useState("");
-   const [team, setTeam] = useState("");
-   const [rank, setRank] = useState("");
-   const [center, setCenter] = useState("");
+  const [today, setToday] = useState(new Date());
+  const [defaultYear, setDefaultYear] = useState("19");
+  const [userData, setUserData] = useState();
+  const [dept, setDept] = useState("");
+  const [team, setTeam] = useState("");
+  const [rank, setRank] = useState("");
+  const [center, setCenter] = useState("");
 
-   useEffect(() => {
-      if(!sabun) return;
-      let postParam = qs.stringify({
-         sabunOrName: sabun
-       });
-   
-       axios.post("http://43.200.115.198:8080/empselect.jsp", postParam).then((response) => {
-           setUserData(response.data.ITEMS[0]);
-           let userInfo = response.data.ITEMS[0];
-   
-           if (
-             today.getFullYear() - 2000 <
-             parseInt(userInfo["identity"].slice(0, 2))
-           ) {
-             setDefaultYear("19");
-           } else {
-             setDefaultYear("20");
-           }
-   
-           GetCDTR.getCDTR(userInfo["center"], userInfo["dept"], userInfo["team"], userInfo["rank"],
-           setCenter, setDept, setTeam, setRank);
-       });
-   }, [sabun])
-   
-  
-   return (
+  useEffect(() => {
+    if (!sabun) return;
+    let postParam = qs.stringify({
+      sabunOrName: sabun,
+    });
+
+    axios
+      .post("http://43.200.115.198:8080/empselect.jsp", postParam)
+      .then((response) => {
+        setUserData(response.data.ITEMS[0]);
+        let userInfo = response.data.ITEMS[0];
+
+        if (
+          today.getFullYear() - 2000 <
+          parseInt(userInfo["identity"].slice(0, 2))
+        ) {
+          setDefaultYear("19");
+        } else {
+          setDefaultYear("20");
+        }
+
+        GetCDTR.getCDTR(
+          userInfo["center"],
+          userInfo["dept"],
+          userInfo["team"],
+          userInfo["rank"],
+          setCenter,
+          setDept,
+          setTeam,
+          setRank
+        );
+      });
+  }, [sabun]);
+
+  return (
     <div ref={componentRef} className="formProofofempMain">
       <div className="perCardtitle">
         <p>재직증명서</p>
+      </div>
+      <table className="formProofofeTable">
+        <tr className="formProofofeFirst">
+          <td>성명</td>
+          <td>{userData ? userData["name"] : ""}</td>
+          <td>생년월일</td>
+          <td>
+            {userData
+              ? defaultYear +
+                userData["identity"].slice(0, 2) +
+                "-" +
+                userData["identity"].slice(2, 4) +
+                "-" +
+                userData["identity"].slice(4, 6)
+              : ""}
+          </td>
+        </tr>
+
+        <tr className="formProofofeFirst">
+          <td>주소</td>
+          <td colSpan={3}>
+            {userData ? userData["address"] : ""}{" "}
+            {userData ? userData["address_detail"] : ""}
+          </td>
+        </tr>
+
+        <tr className="formProofofeFirst">
+          <td>부서</td>
+          <td>{dept}</td>
+          <td>팀</td>
+          <td>{team}</td>
+        </tr>
+
+        <tr className="formProofofeFirst">
+          <td>입사일자</td>
+          <td>
+            {userData
+              ? userData["start_date"].slice(0, 4) +
+                "-" +
+                userData["start_date"].slice(4, 6) +
+                "-" +
+                userData["start_date"].slice(6, 8)
+              : ""}
+          </td>
+          <td>직책</td>
+          <td>{rank}</td>
+        </tr>
+        <tr className="formProofofeFirst formProofofeSecond">
+          <td>용도</td>
+          <td colSpan={3}>제출용</td>
+        </tr>
+      </table>
+
+      <div className="footer_info_form">
+        <p>상기와 같이 재직하고 있음을 증명합니다.</p>
+        <p>
+          {todayTime().slice(0, 4)}년 {todayTime().slice(5, 7)}월{" "}
+          {todayTime().slice(8, 10)}일
+        </p>
+        <p className="mbt">IBO</p>
+        <p>대표이사 담 당 자 &nbsp;&nbsp; (인)</p>
+        <div className="formProofDiv">
+          <img className="formProof" src={IBOstamp} alt="직인" />
         </div>
-        <table className="formProofofeTable">
-            <tr className="formProofofeFirst">
-               <td>성명</td>
-               <td>{userData ? userData["name"] : ""}</td>
-               <td>생년월일</td>
-               <td>{userData ? defaultYear + userData["identity"].slice(0,2) + "-" + userData["identity"].slice(2,4) + "-" + userData["identity"].slice(4, 6) : ""}</td>
-            </tr>
-
-            <tr className="formProofofeFirst">
-               <td>주소</td>
-               <td colSpan={3}>{userData ? userData["address"] : ""} {userData ? userData["address_detail"] : ""}</td>
-            </tr>
-
-            <tr className="formProofofeFirst">
-               <td>연락처</td>
-               <td>{userData ? userData["tel_no"].slice(0, 3) + "-" + userData["tel_no"].slice(3, 7) + "-" + userData["tel_no"].slice(7, 11) : ""}</td>
-               <td>부서</td>
-               <td>{dept}</td>
-            </tr>
-
-            <tr className="formProofofeFirst">
-               <td>입사일자</td>
-               <td>{userData ? userData["start_date"].slice(0, 4) + "-" + userData["start_date"].slice(4, 6) + "-" + userData["start_date"].slice(6, 8) : ""}</td>
-               <td>직책</td>
-               <td>{rank}</td>
-            </tr>
-            <tr className="formProofofeFirst formProofofeSecond">
-               <td>용도</td>
-               <td colSpan={3}>제출용</td>
-            </tr>
-            
-        </table>
-
-         <div className="footer_info_form">
-            <p>상기와 같이 재직하고 있음을 증명합니다.</p>   
-            <p>{todayTime().slice(0, 4)}년 {todayTime().slice(5, 7)}월 {todayTime().slice(8, 10)}일</p>
-            <p className="mbt">IBO</p>
-            <p>대표이사 담 당 자 &nbsp;&nbsp; (인)</p>
-            <div className="formProofDiv">
-               <img className="formProof" src={IBOstamp} alt="직인" />
-            </div>
-         </div>   
+      </div>
     </div>
   );
 };
